da0456985325ab6241b3d9b458e1731c80dd2f0c
Adiákopi Mními <74911623+mnimi@users.noreply.github.com>
2021-01-14 16:52:02 -0500

    [APP] updated entry point.

---
+++ src/main.cr
@@ -0,0 +1,5 @@
+# TODO: Write documentation for Mau program.
+
+module App
+  VERSION = "1.0.0"
+end
--- src/mau.cr
+++
@@ -1,6 +0,0 @@
-# TODO: Write documentation for `Mau`
-module Mau
-  VERSION = "0.1.0"
-
-  # TODO: Put your code here
-end
