9dff741710ef996a634c43e436a5df14b5dc2ebf
Mnimi Aionios <mechild02@gmail.com>
2021-01-15 23:28:17 -0500

    [REBASE] rebasing.

--- src/main.cr
+++
@@ -1,5 +0,0 @@
-# TODO: Write documentation for Mau program.
-
-module App
-  VERSION = "1.0.0"
-end
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+//! Knock-off Among Us game project.
+//!
+//! Written in Rust, using Amethyst as the game engine.
+//!
+//! To build the game, you can run 'cargo build'.
+//!
+//! --BUILD/RUN--------------------------------------------------------
+//! -------------------------------------------------------------------
+//! To start the game in normal mode, you can run 'cargo run -- basic'
+//! To start the game in expert mode, you can run 'cargo run -- expert'
+
+use std::io;
+
+fn main()
+{
+  println!("Hello, world!");
+}
